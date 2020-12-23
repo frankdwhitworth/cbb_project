d100401f00fc77e2f4a08e1bc7fa1fd4c795d47c
Frank Whitworth <frank.d.whitworth.4@gmail.com>
2020-12-23 17:24:16 -0500

    creating main python file

---
+++ main.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+#  . . . . . . . . . . . . . . . . . . . . . . . .
+#  Created on Dec. 23, 2020
+#
+#  @author:     dixon
+#  @file:       main.py   
+#  @summary:    to evaluate the risk of betting on 
+#               different CBB teams
+#  . . . . . . . . . . . . . . . . . . . . . . . . 
+
+
+
+# . . . . . Main Method . . . . .
+# . @s: just the main method    .
+# . . . . . . . . . . . . . . . .
+def main():
+    
+
+
+
+if __name__ == '__main__':
+    main()
