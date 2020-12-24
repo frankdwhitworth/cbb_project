7787b95c0ad7543686dd9b9269e253e5a3533527
Frank Whitworth <frank.d.whitworth.4@gmail.com>
2020-12-24 13:05:38 -0500

    lots of testing

--- main.py
+++ main.py
@@ -10,6 +10,11 @@
 from sportsreference.ncaab.teams import Teams
 from sportsreference.ncaab.boxscore import Boxscore
 from sportsreference.ncaab.schedule import Schedule
+
+from datetime import date
+
+import pandas as pd
+
 # . . . . . Main Method . . . . .
 # . @s: just the main method    .
 # . . . . . . . . . . . . . . . .
@@ -17,18 +22,47 @@ def main():
     print('\n\n\nRyan, this is python. If you see this by command line output, congrats on running your first program on here.')
     print('now onto testing out one of our libraries, sportsreference')
     print(". . .")
-    query_team = "example"
+    query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
     while query_team != 0:
-        query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
+        # getting out of the query field (kinda works)
         if query_team == 0:
             continue
+
+        # getting the schedule for the queried team
         team_schedule = Schedule(query_team)
+        #team_stats = Teams(query_team)
+        #shooting_perc_5(team_schedule)
+
+        '''
+        # printing out different team stats.
+        print('. . . . . . printing team stats . . . . . . . ')
+        # we have to find the team first by going through all the other teams
+        for team in Teams():                  
+            # once this if statement is successfully done, ie. once we find our team
+            if team.name == query_team:
+                # it will print the stats that we want
+                print("team blocks =", team.blocks)
+        print('. . . . . . hope that worked . . . . . . ')
+        '''
+
+        # this will take the schedule that we got from the querried team and now iterrate through each game in schedule
         for game in team_schedule:
-            print(game.date)
-            print(game.result)
-            query_boxscore = game.boxscore
-            print(query_boxscore)
-            print("- - - - - - - - - - - - -")
+            print(game.boxscore.pace)
+            # this is a huge line. this will decide if the game has been played or not...
+            if game.boxscore.pace != None:
+                # if the game boxscore pace IS NOT None, meaning there is a pace, then it will continue to run
+                print(game.date)
+                print(game.result)
+                query_boxscore = game.boxscore
+                print('trying new strat =', query_boxscore.losing_name)
+                #print(query_boxscore)
+                print("- - - - - - - - - - - - -")
+            # if the game boxscore pace IS None, that means there will be no more games
+            else: 
+                print("No more games..")
+                break
+        query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
+
     '''
     # This is an example of how you can use the Teams object (imported in line 10)
     for team in Teams():
@@ -37,7 +71,8 @@ def main():
             print("Duke's turn-over % =", team.turnover_percentage)
     '''
 
-
+def shooting_perc_5(team_sched):
+    print(team_sched)
 
 if __name__ == '__main__':
     main()
