d89ed543d0e4f9ddcd4c23b89b96430429321cbc
Frank Whitworth <frank.d.whitworth.4@gmail.com>
2020-12-24 17:29:34 -0500

    assigning games to an array/list

--- main.py
+++ main.py
@@ -22,17 +22,19 @@ def main():
     print('\n\n\nRyan, this is python. If you see this by command line output, congrats on running your first program on here.')
     print('now onto testing out one of our libraries, sportsreference')
     print(". . .")
-    query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
-    while query_team != 0:
-        # getting out of the query field (kinda works)
-        if query_team == 0:
-            continue
+    query_team = input("Who's scheudle would you like to see? (Enter 0 to exit) \n> ")
+    while query_team != '0':
 
         # getting the schedule for the queried team
         team_schedule = Schedule(query_team)
         #team_stats = Teams(query_team)
         #shooting_perc_5(team_schedule)
 
+        # setting up local arrays for the team we just queried
+        past_played_games = []
+        past_five_games = []
+        tot_games_played = 0
+        
         '''
         # printing out different team stats.
         print('. . . . . . printing team stats . . . . . . . ')
@@ -41,27 +43,36 @@ def main():
             # once this if statement is successfully done, ie. once we find our team
             if team.name == query_team:
                 # it will print the stats that we want
-                print("team blocks =", team.blocks)
+                tot_games_played = team.games_played
+                print("total games played =", tot_games_played)
         print('. . . . . . hope that worked . . . . . . ')
         '''
 
         # this will take the schedule that we got from the querried team and now iterrate through each game in schedule
         for game in team_schedule:
-            print(game.boxscore.pace)
+            #print(game.boxscore.pace)
             # this is a huge line. this will decide if the game has been played or not...
             if game.boxscore.pace != None:
+                past_played_games.append(game)
                 # if the game boxscore pace IS NOT None, meaning there is a pace, then it will continue to run
-                print(game.date)
-                print(game.result)
+                #print(game.date)
+                #print(game.result)
                 query_boxscore = game.boxscore
-                print('trying new strat =', query_boxscore.losing_name)
+                #print('trying new strat =', query_boxscore.losing_name)
                 #print(query_boxscore)
-                print("- - - - - - - - - - - - -")
+                print("- game saved -")
             # if the game boxscore pace IS None, that means there will be no more games
             else: 
                 print("No more games..")
                 break
-        query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
+        
+        tot_games_played = len(past_played_games)
+        print("Now we will be printing the games played (well, testing with the date) - should be", tot_games_played)
+        for game in past_played_games:
+            print(game.date)
+        
+        
+        query_team = input("\nWho's scheudle would you like to see? (Enter 0 to exit) \n> ")
 
     '''
     # This is an example of how you can use the Teams object (imported in line 10)
