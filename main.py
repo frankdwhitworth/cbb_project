b5f889f46ff911821420f9a26af3c0759a0845d7
Frank Whitworth <frank.d.whitworth.4@gmail.com>
2020-12-23 19:57:06 -0500

    great progress on sportsref lib

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 #  . . . . . . . . . . . . . . . . . . . . . . . .
 #  Created on Dec. 23, 2020
 #
@@ -9,15 +7,35 @@
 #               different CBB teams
 #  . . . . . . . . . . . . . . . . . . . . . . . . 
 
-
-
+from sportsreference.ncaab.teams import Teams
+from sportsreference.ncaab.boxscore import Boxscore
+from sportsreference.ncaab.schedule import Schedule
 # . . . . . Main Method . . . . .
 # . @s: just the main method    .
 # . . . . . . . . . . . . . . . .
 def main():
-    print ('Ryan, this is python. If you see this by command line output, congrats on running your first program on here.')
-
-    
+    print('\n\n\nRyan, this is python. If you see this by command line output, congrats on running your first program on here.')
+    print('now onto testing out one of our libraries, sportsreference')
+    print(". . .")
+    query_team = "example"
+    while query_team != 0:
+        query_team = input("Who's scheudle would you like to see? (Enter 0 to exit)")
+        if query_team == 0:
+            continue
+        team_schedule = Schedule(query_team)
+        for game in team_schedule:
+            print(game.date)
+            print(game.result)
+            query_boxscore = game.boxscore
+            print(query_boxscore)
+            print("- - - - - - - - - - - - -")
+    '''
+    # This is an example of how you can use the Teams object (imported in line 10)
+    for team in Teams():
+        if team.name == 'Duke':
+            print(team.name, team.wins, team.losses)
+            print("Duke's turn-over % =", team.turnover_percentage)
+    '''
 
 
 
